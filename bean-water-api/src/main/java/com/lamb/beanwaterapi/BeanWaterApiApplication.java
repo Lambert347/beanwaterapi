8f318fc45d06cc369ea26f3556f5888874fa2439
Ian Lambert <lambe347@umn.edu>
2023-11-13 15:41:24 -0600

    created base spring application

---
+++ bean-water-api/src/main/java/com/lamb/beanwaterapi/BeanWaterApiApplication.java
@@ -0,0 +1,15 @@
+package com.lamb.beanwaterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeanWaterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeanWaterApiApplication.class, args);
+
+		
+	}
+
+}
---
+++ bean-water-api/src/test/java/com/lamb/beanwaterapi/BeanWaterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lamb.beanwaterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeanWaterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
